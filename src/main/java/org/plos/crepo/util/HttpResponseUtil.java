cd64efc6f635a6fd6db2b3e1b8b78e16f8dbc6e9
Ryan Skonnord <rskonnord@plos.org>
2015-03-20 16:10:13 -0700

    DPRO-917: Don't return null on HttpResponseUtil exceptions

--- src/main/java/org/plos/crepo/util/HttpResponseUtil.java
+++ src/main/java/org/plos/crepo/util/HttpResponseUtil.java
@@ -6,43 +6,34 @@ import org.apache.commons.lang3.CharEncoding;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Utility class used to handle the Http responses.
  */
 public class HttpResponseUtil {
 
-  private static final Logger log = LoggerFactory.getLogger(HttpResponseUtil.class);
-
   public static String getResponseAsString(HttpResponse response) {
-
+    HttpEntity entity = response.getEntity();
     try {
-
-      HttpEntity entity = response.getEntity();
       return EntityUtils.toString(entity, CharEncoding.UTF_8);
-
-    } catch (Exception e) {
-      log.error("error streaming to string", e);
-      return null;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 
   public static String getErrorMessage(HttpResponse response) {
-
+    HttpEntity entity = response.getEntity();
+    String responseMessage;
     try {
-
-      HttpEntity entity = response.getEntity();
-      String responseMessage =  EntityUtils.toString(entity, CharEncoding.UTF_8);
-      Gson gson = new Gson();
-      JsonObject jsonObject = gson.fromJson(responseMessage, JsonObject.class);
-      return jsonObject.get("message").getAsString();
-
-    } catch (Exception e) {
-      log.error("error streaming to string", e);
-      return null;
+      responseMessage = EntityUtils.toString(entity, CharEncoding.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+    Gson gson = new Gson();
+    JsonObject jsonObject = gson.fromJson(responseMessage, JsonObject.class);
+    return jsonObject.get("message").getAsString();
   }
 
 }
