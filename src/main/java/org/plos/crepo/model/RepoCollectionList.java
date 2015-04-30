5286597826ada677751c86545a94e4916f8cd8fe
Mamta Singh <msingh@plos.org>
2015-04-30 15:28:21 -0700

    DPRO-955

--- src/main/java/org/plos/crepo/model/RepoCollectionList.java
+++ src/main/java/org/plos/crepo/model/RepoCollectionList.java
@@ -9,19 +9,16 @@ import java.util.Map;
 /**
  * Represents output to the client, describing a collection.
  */
-public class RepoCollectionObjectsMetadata extends RepoMetadata {
+public class RepoCollectionList extends RepoMetadata {
 
   private final ImmutableList<RepoObjectMetadata> objects;
 
-  public RepoCollectionObjectsMetadata(Map<String, Object> raw) {
+  public RepoCollectionList(Map<String, Object> raw) {
     super(raw);
     objects = parseObjects(raw);
   }
 
   private static ImmutableList<RepoObjectMetadata> parseObjects(Map<String, Object> raw) {
-    if (!raw.containsKey("objects")) {
-      return null;
-    }
     List<Map<String, Object>> rawObjects = (List<Map<String, Object>>) raw.get("objects");
     List<RepoObjectMetadata> builtObjects = new ArrayList<>(rawObjects.size());
     for (Map<String, Object> rawObject : rawObjects) {
--- src/main/java/org/plos/crepo/model/RepoCollectionMetadata.java
+++ src/main/java/org/plos/crepo/model/RepoCollectionMetadata.java
@@ -11,24 +11,8 @@ import java.util.Map;
  */
 public class RepoCollectionMetadata extends RepoMetadata {
 
-  private final ImmutableList<RepoObjectMetadata> objects;
-
   public RepoCollectionMetadata(Map<String, Object> raw) {
     super(raw);
-    objects = parseObjects(raw);
-  }
-
-  private static ImmutableList<RepoObjectMetadata> parseObjects(Map<String, Object> raw) {
-    List<Map<String, Object>> rawObjects = (List<Map<String, Object>>) raw.get("objects");
-    List<RepoObjectMetadata> builtObjects = new ArrayList<>(rawObjects.size());
-    for (Map<String, Object> rawObject : rawObjects) {
-      builtObjects.add(new RepoObjectMetadata(rawObject));
-    }
-    return ImmutableList.copyOf(builtObjects);
-  }
-
-  public ImmutableList<RepoObjectMetadata> getObjects() {
-    return objects;
   }
 
 }
